59560dd724aef102ea177f8aea3365151450c35f
Nirmalapasupuleti <113580438+Nirmalapasupuleti@users.noreply.github.com>
2022-12-15 17:14:35 +0530

    Add files via upload

--- Zombie.c
+++ Zombie.c
@@ -3,10 +3,13 @@
 #include<unistd.h>
 int main()
 {
+	//CHILD
 	if(fork()==0)
 	{
+		//getting a id of chaild pid and parent ppid
 		printf("in child pid=%d ppid=%d\n",getpid(),getppid());
 		sleep(10);
+		//After 10 seconds child will be termanating
 		printf("child is terminating\n");
 	}
 	else
