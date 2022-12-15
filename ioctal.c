c2740591ace7d9290953f2bdfcac438e7a95ec41
Nirmalapasupuleti <113580438+Nirmalapasupuleti@users.noreply.github.com>
2022-12-15 16:28:22 +0530

    Add files via upload

---
+++ ioctal.c
@@ -0,0 +1,23 @@
+//Write a program show the /dev/input/event0 device name?
+#include<stdio.h>
+#include <fcntl.h>
+#include<sys/ioctl.h>
+#include <linux/input.h>
+int main(int argc, char *argv[])
+{
+	int fd1,retfd;
+	char buf[4096];
+	int val;
+	char name[256] ;
+	//argv[1] has in format /dev/input/event0
+	fd1 = open(argv[1],O_RDONLY);
+	if(fd1<2)
+	{
+		printf("Open Fails");
+		return -1;
+	}
+	//function call to get device name
+	ioctl(fd1,EVIOCGNAME(sizeof(name)),name);
+	printf("Input device name: \"%s\"\n",name);
+	return 0;
+}
